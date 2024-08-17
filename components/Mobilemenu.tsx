f70da16008ffac7cfcf4ee81799c2e9fc2c17286
Kyle <johnkyleobusan@gmail.com>
2024-08-17 18:48:27 +0800

    fix border

--- components/Mobilemenu.tsx
+++ components/Mobilemenu.tsx
@@ -7,7 +7,7 @@ const Mobilemenu: React.FC<MobileMenuProps> = ({visible}) => {
         return null;
     }
   return (
-    <div className='bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 flex'>
+    <div className='bg-black w-56 absolute top-8 left-0 py-5 flex-col border-gray-800 flex z-10 bg-opacity-50 border-t-2 border-t-slate-50'>
         <div className='flex flex-col gap-4'>
             <div className='px-3 text-center text-white hover:underline'>
                 Home
